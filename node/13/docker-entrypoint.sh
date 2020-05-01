987de5607e53e1d98fa90399ada3c54433e41f4f
Dennis de Best <dennis@debest.fr>
2020-05-01 14:43:58 +0200

    Start pm2 container

---
+++ node/13/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+if [ "$1" = 'start' ]; then
+  if [ "$2" = 'nuxt' ]; then
+    yarn build
+    pm2 start --no-daemon
+  else
+    pm2 start ${@:2}
+  fi
+elif [ "$1" = 'restart' ]; then
+    pm2 restart ${@:2}
+  fi
+else
+  exec "$@"
+fi
+
+
